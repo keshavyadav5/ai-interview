e686df64dcc66d987d65e811dc3f702d26406d4c
Keshav <117563969+keshavyadav5@users.noreply.github.com>
2026-02-27 16:08:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ import cors from 'cors'
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ai-interview-1clinet.onrender.com",
   credentials: true
 }))
 
